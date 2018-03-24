9b8b5e4425223fe7e65bac9ff4fc3809a1246bf3
Mishal <mishgit95@gmail.com>
2018-03-24 22:35:43 +0530

    Tooltip fixed

--- lib/charts/LineChart.jsx
+++ lib/charts/LineChart.jsx
@@ -53,14 +53,17 @@ var ToolTip=React.createClass({
     propTypes: {
         tooltip:React.PropTypes.object
     },
+    // #e54e16
     render:function(){
         var visibility="hidden";
         var transform="";
+        var fill ="";
         var x=0;
         var y=0;
         var width=150,height=70;
         var transformText='translate('+width/2+','+(height/2-5)+')';
         var transformArrow="";
+        
         if(this.props.tooltip.display==true){
             var position = this.props.tooltip.pos;
             x= position.x;
@@ -70,7 +73,6 @@ var ToolTip=React.createClass({
                 transform='translate(' + (x-width/2) + ',' + (y-height-20) + ')';
                 transformArrow='translate('+(width/2-20)+','+(height-2)+')';
             }else if(y<height){
-
                 transform='translate(' + (x-width/2) + ',' + (Math.round(y)+20) + ')';
                 transformArrow='translate('+(width/2-20)+','+0+') rotate(180,20,0)';
             }
@@ -79,12 +81,12 @@ var ToolTip=React.createClass({
         }
         return (
             <g transform={transform}>
-                <rect class="shadow" is width={width} height={height} rx="5" ry="5" visibility={visibility} fill="#6391da" opacity=".9"/>
+                <rect class="shadow" is width={width} height={height} rx="5" ry="5" visibility={visibility} fill={(this.props.tooltip.data.value > this.props.SLA[0].count) ? "#e54e16" :"#6391da"} opacity=".9"/>
                 <polygon class="shadow" is points="10,0  30,0  20,10" transform={transformArrow}
                          fill="#6391da" opacity=".9" visibility={visibility}/>
                 <text is visibility={visibility} transform={transformText}>
                     <tspan is x="0" text-anchor="middle" font-size="15px" fill="#ffffff">{this.props.tooltip.data.key}</tspan>
-                    <tspan is x="0" text-anchor="middle" dy="25" font-size="20px" fill="#a9f3ff">{this.props.tooltip.data.value+" visits"}</tspan>
+                    <tspan is x="0" text-anchor="middle" dy="25" font-size="20px" fill={(this.props.tooltip.data.value > this.props.SLA[0].count) ? "#ffffff" :"#a9f3ff"}>{this.props.tooltip.data.value+":00"}</tspan>
                 </text>
             </g>
         );
@@ -232,7 +234,7 @@ var LineChart=React.createClass({
 
                         <Dots data={this.props.data} x={x} y={y} showToolTip={this.showToolTip} hideToolTip={this.hideToolTip}/>
                                    
-                        <ToolTip tooltip={this.state.tooltip}/>
+                        <ToolTip tooltip={this.state.tooltip} SLA={this.props.SLA}/>
                     </g>
 
                 </svg>
