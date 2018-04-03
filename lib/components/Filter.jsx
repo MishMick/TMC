bdd38a0873282ae469358ff3c311df1d024c409c
Mishal <mishgit95@gmail.com>
2018-04-04 01:17:10 +0530

    CSS changes

--- lib/components/Filter.jsx
+++ lib/components/Filter.jsx
@@ -30,30 +30,49 @@ var Filter = React.createClass({
         return (
             <div className="filterDiv">
                 <form className="filterOptions">
-                    <div className="col-cs-12">
-
+                    <div className="col-xs-12">
                         <h4>File types</h4>
-                        <div className="col-xs-10">
-                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="apple" /><h5>Apple</h5>
-                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="orange" /><h5>Orange</h5>
-                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="watermelon" /><h5>Watermelon</h5>
-                        </div>
-                        <br />
+                            <div className="checkBoxGroup">
+                            <div className="row">
+                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="pnl_attrib" />PNL ATTRIB
+                            </div>
+                            <div className="row">
+                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="mtm_cash" />MTM CASH
+                            </div>
+                            <div className="row">
+                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="risq" />RISQ
+                            </div>
+                            <div className="row">
+                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="new_trade" />NEW TRADE
+                            </div>
+                            <div className="row">
+                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="flex_swap" />FLEX SWAP
+                            </div>
+                            <div className="row">
+                            <input onChange={this.handleFileTypeChange} type="checkbox" name="file_type" value="amend" />AMEND
+                            </div>
+                            </div>
+                        <hr />
 
                         <h4>Location</h4>
-                        <div className="col-xs-12">
-                            <input onChange={this.handleLocationChange} type="radio" name="location" value="London" /><h5>London</h5>
-                            <input onChange={this.handleLocationChange} type="radio" name="location" value="Paris" /><h5>Paris</h5>
-                            <input onChange={this.handleLocationChange} type="radio" name="location" value="New York" /><h5>New York</h5>
+                        <div className="radioGroup">
+                            <div className="row">
+                            <input onChange={this.handleLocationChange} type="radio" name="location" value="London" />London
+                            </div>
+                            <div className="row">
+                            <input onChange={this.handleLocationChange} type="radio" name="location" value="Paris" />Paris
+                            </div>
+                            <div className="row">
+                            <input onChange={this.handleLocationChange} type="radio" name="location" value="New York" />New York
+                            </div>
                         </div>
-                        <br />
-                        <br />
-
+                        <hr />
+                    
                         <h4>Date range</h4>
-                        <div className="col-xs-10">
+                       
                             <input className="form-control" onChange={this.handleStartDateChange} id="startDate" name="startDate" type="date" min={this.props.startDate} max={this.props.endDate} value={this.state.startDate} /> <h5>to</h5>
                             <input className="form-control" onChange={this.handleEndDateChange} id="endDate" name="endDate" type="date" min={this.props.startDate} max={this.props.endDate} value={this.state.endDate} />
-                        </div>
+                    
                     </div>
                 </form>
             </div>
